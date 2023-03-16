cd4ee1f694960a77710ecf7e411da064792c325f
djametcode <kampretsiji1@gmail.com>
2023-03-17 02:08:23 +0700

    changes

--- frontend/digitalstore/src/components/foryouproduct.jsx
+++ frontend/digitalstore/src/components/foryouproduct.jsx
@@ -36,7 +36,7 @@ const Extends = ({ data }) => {
       <div>
         {/* <ImageProducts data={images} /> */}
         <div className=" flex justify-center pt-2">
-          <div className=" w-40 h-44 bg-cover bg-dummy rounded-lg"></div>
+          <div className=" w-40 h-40 bg-cover bg-dummy rounded-lg"></div>
         </div>
       </div>
 
